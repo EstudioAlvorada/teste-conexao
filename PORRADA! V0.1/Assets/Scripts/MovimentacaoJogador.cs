5508c6781d9738333ae32a908846ba07f8772888
RenanSN <renan.nascimento98@outlook.com>
2020-02-24 22:43:19 -0300

    V1

---
+++ PORRADA! V0.1/Assets/Scripts/MovimentacaoJogador.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentacaoJogador : MonoBehaviour
+{
+
+    [SerializeField] private float mov;
+    [SerializeField] private float velocMov = 0f;
+    [SerializeField] private float velocPulo = 0f;
+    [SerializeField] private GameObject checaChao;
+    [SerializeField] private LayerMask camadaChao;
+    bool noChao;
+
+    [SerializeField] float velocQueda = 2.5f;
+    [SerializeField] float multiplicadorPulinho = 2f;
+
+
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+
+    protected bool jump;
+    Rigidbody2D corpo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        corpo = GetComponent<Rigidbody2D>();
+
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        noChao = Physics2D.Linecast(checaChao.transform.position, corpo.transform.position, camadaChao);
+
+        mov = joystick.Horizontal;
+
+        corpo.velocity = new Vector2(mov * velocMov, corpo.velocity.y);
+
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (!jump && joybutton.Pressed && noChao)
+        {
+            jump = true;
+            corpo.velocity += Vector2.up * velocPulo;
+        }
+
+        if (jump && !joybutton.Pressed)
+        {
+            jump = false;
+        }
+
+        if(corpo.velocity.y < 0)
+        {
+            corpo.velocity += Vector2.up * Physics2D.gravity.y * (velocQueda - 1) * Time.deltaTime;
+        }
+        else if(corpo.velocity.y > 0 && !jump)
+        {
+            corpo.velocity += Vector2.up * Physics2D.gravity.y * (multiplicadorPulinho - 1) * Time.deltaTime;
+
+        }
+
+
+    }
+}
---
+++ PORRADA! V0.1/Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Cinemachine;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private CinemachineVirtualCamera cameraJogador = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        var player = PhotonNetwork.Instantiate("Jogador", new Vector2(Random.Range(-8f, 8f) , transform.position.y), Quaternion.identity);
+        cameraJogador.Follow = player.transform;
+        //cameraJogador.LookAt = player.transform;
+    }
+}
